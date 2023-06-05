b94612c5df732d1212807370d41dfe5c4804dda3
luizfelipe25 <luiz.farias.cpv@gmail.com>
2023-06-05 13:32:52 -0300

    initial commit

---
+++ src/main/java/com/freedom/wishlist/WishlistApplication.java
@@ -0,0 +1,19 @@
+package com.freedom.wishlist;
+
+import com.freedom.wishlist.core.entities.Wishlist;
+import com.freedom.wishlist.infrastructure.repository.WishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class WishlistApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freedom/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freedom.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
